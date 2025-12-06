f275ce37d77601c4d57229db1df361df77e4f811
ukp1eeee <gijiom596@gmail.com>
2025-12-06 22:51:30 +0200

    Migrated from nextcord to Discord.py, and changed some bugs with token loading

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import nextcord
-from nextcord.ext import commands
+import discord
+from discord.ext import commands
 
 
-with open("token.txt") as token:
-    token.read()
+with open("token.txt", "r", encoding="utf-8") as f:
+    token = f.read().strip()
 
 
-intents = nextcord.Intents.all()
-bot = commands.Bot(command_prefix="/", intents=nextcord.intents.all)
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 
 @bot.hybrid_command()
 async def ping(ctx):
-    await ctx.send(f"Pong! You succesfully activated Discord Active Developer Badge")
+    await ctx.send(f"Pong! You succesfully activated Discord Active Developer Badge") #You succesfully activated Discord Developer Badge!
 
 @bot.event
 async def on_ready():
